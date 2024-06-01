f5f623d97001ec4b0f0c80a9fd5fc6d1351fd04e
Harshal Mali <114692074+harshal20m@users.noreply.github.com>
2024-06-01 18:01:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,4 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/attendance", require("./routes/attendance"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port  ${PORT}`));
