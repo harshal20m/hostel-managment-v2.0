e750a140cda200b1de2bdea02b4e3b5b3ec2bd37
Harshal Mali <114692074+harshal20m@users.noreply.github.com>
2024-06-01 18:00:48 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = function (req, res, next) {
 	const token = req.header("x-auth-token");
 	if (!token) {
-		return res.status(401).json({ msg: "No token, authorization denied" });
+		return res.status(401).json({ msg: "No token,authorization denied" });
 	}
 	try {
 		const decoded = jwt.verify(token, process.env.SECRET);
