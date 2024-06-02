a175ad04f1059029998a865bd41582a954e10550
Harshal Mali <114692074+harshal20m@users.noreply.github.com>
2024-06-03 00:25:25 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
 			<h1 className="text-4xl font-bold text-gray-800">Hostel Management System</h1>
 			<Link
 				to="/dashboard"
-				className="inline-block mt-5 px-5 py-3 text-white bg-blue-500 rounded hover:bg-blue-700"
+				className="inline-block  mt-5 px-5 py-3 text-white bg-blue-500 rounded hover:bg-blue-700"
 			>
 				Go to Dashboard
 			</Link>
